d3befba5d082269214bfe6ecbdee1863f6bfe111
Ivan Kanashov <ikanashov@gmail.com>
2021-03-28 18:26:37 +0500

    Add get_all_film from elastic

--- src/services/film.py
+++ src/services/film.py
@@ -1,5 +1,7 @@
+import logging
+import logging.config
 from functools import lru_cache
-from typing import Optional
+from typing import List, Optional
 
 from aioredis import Redis
 
@@ -7,14 +9,20 @@ from elasticsearch import AsyncElasticsearch
 
 from fastapi import Depends
 
+from core.config import config
+from core.logger import LOGGING
+
 from db.elastic import get_elastic
 from db.redis import get_redis
 
 from models.film import SFilm
 
-
+# need to remove magic number
 FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5
 
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger('root')
+logger.debug('Start logging')
 
 # FilmService содержит бизнес-логику по работе с фильмами.
 # Никакой магии тут нет. Обычный класс с обычными методами.
@@ -36,8 +44,21 @@ class FilmService:
                 return None
             # Сохраняем фильм  в кеш
             await self._put_film_to_cache(film)
-
         return film
+
+    # Здесь же будем пытаться кэшировать и брать из кэша
+    async def get_all_film(self, sort: str, page_size: int, page_number: int) -> Optional[List[SFilm]]:
+        film = await self._get_all_film_from_elastic(sort, page_size, page_number)
+        return film
+
+    async def _get_all_film_from_elastic(self, sort: str, page_size: int, page_number: int) -> Optional[List[SFilm]]:
+        from_  = page_size * (page_number-1)
+        # Подумать а стоит ли проверять на наличие правильного индекса, если индекс пустой то все работает
+        # а вот если не существует то ошибка 404 надо ли ее обрабатывать ? подумать
+        docs = await self.elastic.search(index=config.ELASTIC_INDEX, sort=sort, size=page_size, from_=from_)
+        films = [SFilm(**doc['_source']) for doc in docs['hits']['hits']]
+        #logger.debug(films)
+        return films
 
     async def _get_film_from_elastic(self, film_id: str) -> Optional[SFilm]:
         doc = await self.elastic.get('movies', film_id)
