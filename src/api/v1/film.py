c7eb4565787778eaddb2542842b03eca9943d59e
Ivan Kanashov <ikanashov@gmail.com>
2021-03-28 18:32:18 +0500

    Add get_all_film sort, page_size, page-number query parameters

--- src/api/v1/film.py
+++ src/api/v1/film.py
@@ -1,8 +1,9 @@
 from http import HTTPStatus
+from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 
-from api.v1.models import FilmDetail
+from api.v1.models import FilmDetail, FilmShort, FilmSort
 
 from services.film import FilmService, get_film_service
 
@@ -10,6 +11,30 @@ from services.film import FilmService, get_film_service
 # Объект router, в котором регистрируем обработчики
 router = APIRouter()
 
+
+@router.get('/test', response_model=List[FilmShort])
+async def get_all_film(
+    sort: FilmSort = Query(
+        FilmSort.imdb_rating_desc_alias,
+        title='Sort field',
+        description='Sort field (default: "-imdb_rating", sort by imdb_rating in descending order)'
+        ),
+    # I want to be like this page: DictPage = Query(...), but seems it doesn't alowed by pydantic and fastapi
+    # I want use dict like page[number], page[size] as query parameters
+    # https://github.com/tiangolo/fastapi/issues/884
+    # https://github.com/tiangolo/fastapi/issues/1415
+    # remove magic number 1, 50
+    page_size:  int = Query(50, alias='page[size]', ge = 1),
+    page_number: int = Query(1, alias='page[number]', ge = 1),    
+    film_service: FilmService = Depends(get_film_service)
+    ):
+    if sort == FilmSort.imdb_rating_asc_alias:
+        sort = FilmSort.imdb_rating_asc
+    if sort == FilmSort.imdb_rating_desc_alias:
+        sort = FilmSort.imdb_rating_desc
+    films = await film_service._get_all_film_from_elastic(sort, page_size , page_number)
+    films = [FilmShort(**film.dict(by_alias=True)) for film in films]
+    return films
 
 # Внедряем FilmService с помощью Depends(get_film_service)
 @router.get('/{film_id}', response_model=FilmDetail)
