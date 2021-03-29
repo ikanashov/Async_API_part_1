f65bc1c863c511672f53e81c2876668e519ec1e7
Ivan Kanashov <ikanashov@gmail.com>
2021-03-29 14:21:32 +0500

    Add Page and FilmSort class

--- src/api/v1/film.py
+++ src/api/v1/film.py
@@ -3,7 +3,7 @@ from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException, Query
 
-from api.v1.models import FilmDetail, FilmShort, FilmSort
+from api.v1.models import FilmDetail, FilmShort, FilmSort, Page
 
 from services.film import FilmService, get_film_service
 
@@ -12,28 +12,13 @@ from services.film import FilmService, get_film_service
 router = APIRouter()
 
 
-@router.get('/test', response_model=List[FilmShort])
+@router.get('', response_model=List[FilmShort])
 async def get_all_film(
-    sort: FilmSort = Query(
-        FilmSort.imdb_rating_desc_alias,
-        title='Sort field',
-        description='Sort field (default: "-imdb_rating", sort by imdb_rating in descending order)'
-        ),
-    # I want to be like this page: DictPage = Query(...), but seems it doesn't alowed by pydantic and fastapi
-    # I want use dict like page[number], page[size] as query parameters
-    # https://github.com/tiangolo/fastapi/issues/884
-    # https://github.com/tiangolo/fastapi/issues/1415
-    # remove magic number 1, 50
-    page_size:  int = Query(50, alias='page[size]', ge=1),
-    page_number: int = Query(1, alias='page[number]', ge=1),
+    sort: FilmSort = Depends(),
+    page: Page = Depends(),
     film_service: FilmService = Depends(get_film_service)
-    ):
-
-    if sort == FilmSort.imdb_rating_asc_alias:
-        sort = FilmSort.imdb_rating_asc
-    if sort == FilmSort.imdb_rating_desc_alias:
-        sort = FilmSort.imdb_rating_desc
-    films = await film_service._get_all_film_from_elastic(sort, page_size, page_number)
+):
+    films = await film_service._get_all_film_from_elastic(sort.sort, page.page_size, page.page_number)
     films = [FilmShort(**film.dict(by_alias=True)) for film in films]
     return films
 
--- src/api/v1/models.py
+++ src/api/v1/models.py
@@ -3,6 +3,8 @@ from typing import List, Optional, TypedDict
 
 from pydantic.types import UUID4
 
+from fastapi import Query
+
 from core.orjson import BaseModelOrjson
 
 
@@ -20,6 +22,7 @@ class FilmPerson(BaseModelOrjson):
 class FilmShort(BaseModelOrjson):
     uuid: UUID4
     title: str
+    description: Optional[str]
     imdb_rating: float
 
 
@@ -41,14 +44,35 @@ class DictPage(TypedDict):
     size: int
     number: int
 
+# remove magic number 1, 50
+class Page:
+    def __init__(
+        self,
+        page_size:  int = Query(50, alias='page[size]', ge=1),
+        page_number: int = Query(1, alias='page[number]', ge=1)
+    ) -> None:
+        self.page_size = page_size
+        self.page_number = page_number
 
-# unused class remove it
-class Page(BaseModelOrjson):
-    page: DictPage
 
-
-class FilmSort(str, Enum):
+class FilmSortEnum(str, Enum):
     imdb_rating_asc: str = 'imdb_rating:asc'
     imdb_rating_asc_alias: str = 'imdb_rating'
     imdb_rating_desc: str = 'imdb_rating:desc'
     imdb_rating_desc_alias: str = '-imdb_rating'
+
+
+class FilmSort:
+    def __init__(
+        self,
+        sort: FilmSortEnum = Query(
+            FilmSortEnum.imdb_rating_desc_alias,
+            title='Sort field',
+            description='Sort field (default: "-imdb_rating", sort by imdb_rating in descending order)'
+        ) 
+    ) -> None:
+        if sort == FilmSortEnum.imdb_rating_asc_alias:
+            sort = FilmSortEnum.imdb_rating_asc
+        if sort == FilmSortEnum.imdb_rating_desc_alias:
+            sort = FilmSortEnum.imdb_rating_desc
+        self.sort = sort
