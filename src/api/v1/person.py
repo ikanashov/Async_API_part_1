f26a327de05316fc79d31cdb99ba832380ee29a2
Ivan Kanashov <ikanashov@gmail.com>
2021-04-12 23:05:00 +0500

    add get films by person_id

--- src/api/v1/person.py
+++ src/api/v1/person.py
@@ -1,9 +1,10 @@
 from http import HTTPStatus
+from logging import log
 from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException
 
-from api.v1.models import FilmPersonDetail, FilmPersonQuery, FilmPersonSort, Page
+from api.v1.models import FilmPersonDetail, FilmPersonQuery, FilmPersonSort, FilmShort, Page
 
 from services.film import FilmService, get_film_service
 
@@ -22,7 +23,6 @@ async def get_all_person(
     persons = [FilmPersonDetail(**person.dict(by_alias=True)) for person in persons]
     return persons
 
-
 @router.get('/search', response_model=List[FilmPersonDetail])
 async def search_person(
     query: FilmPersonQuery = Depends(),
@@ -37,5 +37,13 @@ async def search_person(
 async def person_details(person_id: str, film_service: FilmService = Depends(get_film_service)) -> FilmPersonDetail:
     person = await film_service.get_person_by_id(person_id)
     if not person:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='person not found')
     return FilmPersonDetail(**person.dict(by_alias=True))
+
+@router.get('/{person_id}/film', response_model=List[FilmShort], deprecated=True)
+async def films_by_person(person_id: str, film_service: FilmService = Depends(get_film_service)) -> List[FilmShort]:
+    person = await film_service.get_person_by_id(person_id)
+    if not person:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='person not found')
+    films = [await film_service.get_by_id(film_id) for film_id in person.filmids]
+    return films
