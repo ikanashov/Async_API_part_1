148abb045c7f9d3442b531dae7bd918107d10748
Ivan Kanashov <ikanashov@gmail.com>
2021-04-19 22:28:50 +0500

    backup restore all indexes

---
+++ elastic/backup/restore_all_index.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Backup all used index in elastic for next task
+
+ELASTIC_INDEX='movies,genres,persons'
+BACKUP_LOCATION='alldata'
+
+echo 'Restore backup for' ${ELASTIC_INDEX} 'index'
+
+if [ -z $ELASTIC_USER ]; then ELASTIC_USER='elastic'; fi
+AUTH="--user ${ELASTIC_USER}:${ELASTIC_PASSWORD}"
+
+PORT=$(env | grep -oP "^http\.port=\K.*")
+if [ -z $PORT ]; then	PORT=9200; fi
+
+curl $AUTH -X PUT "localhost:${PORT}/_snapshot/${BACKUP_LOCATION}?pretty" -H 'Content-Type: application/json' -d'
+{
+  "type": "fs",
+  "settings": {
+    "location": "'${BACKUP_LOCATION}'",
+    "compress": true
+  }
+}
+'
+
+echo "Restore current snapshot"
+curl $AUTH -X POST \
+"localhost:${PORT}/_snapshot/${BACKUP_LOCATION}/current/_restore?pretty" \
+-H 'Content-Type: application/json' -d'
+{
+  "indices": "'${ELASTIC_INDEX}'",
+  "ignore_unavailable": true,
+  "include_global_state": true
+}
+'
